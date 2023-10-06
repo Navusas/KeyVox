f2f1f3886c0400f5c311fba628819a78cdb6615b
Navusas <D.Giedraitis7@gmail.com>
2023-10-06 20:22:38 +0100

    Clipboard kind of works

--- OsSpecificImplementation/KeyVox.OsSpecific.Windows.App/Program.cs
+++ OsSpecificImplementation/KeyVox.OsSpecific.Windows.App/Program.cs
@@ -20,7 +20,7 @@ namespace KeyVox.OsSpecific.Windows.App
                 icon.ContextMenuStrip = new ContextMenuStrip();
                 icon.ContextMenuStrip.Items.Add("Exit", null, (s, e) => Application.Exit());
 
-                HotkeyManager.Current.AddOrReplace("ShiftZHotkey", Keys.Shift | Keys.Z, OnHotkeyPressed);
+                HotkeyManager.Current.AddOrReplace("ShiftZHotkey", Keys.Alt | Keys.A, OnHotkeyPressed);
 
                 Application.Run();
             }
@@ -32,14 +32,52 @@ namespace KeyVox.OsSpecific.Windows.App
             {
                 _isOpen = true;
 
+                string selectedText = GetCurrentlySelectedText();
+
                 // Handle the hotkey press here, e.g., show a Tauri window or another form.
-                MessageBox.Show("SHIFT + Z pressed!");
+                MessageBox.Show($"SHIFT + Z pressed!\nSelected Text: {selectedText}");
 
                 _isOpen = false;
-
             }
 
             e.Handled = true;
+        }
+
+        private static string GetCurrentlySelectedText()
+        {
+            // Backup the current clipboard content
+            IDataObject clipboardBackup = Clipboard.GetDataObject();
+            string initialClipboardText = Clipboard.GetText();
+
+
+            // Simulate Ctrl+C to copy selected text to clipboard
+            SendKeys.SendWait("^c");
+
+
+            //// Clipboard update takes time. We
+            //// want to poll for clipboard update (up to a timeout)
+            string selectedText = string.Empty;
+            int attemptLimit = 200;
+            int attempts = 200;
+            for (int i = 0; i < attemptLimit; i++)
+            {
+                selectedText = Clipboard.GetText();
+                if (selectedText != initialClipboardText)
+                {
+                    break;
+                }
+                Thread.Sleep(10);
+                attempts--;
+            }
+
+            selectedText += "\n\n attempt " + attempts.ToString();
+
+
+            // Restore clipboard from backup
+            if (clipboardBackup != null)
+                Clipboard.SetDataObject(clipboardBackup);
+
+            return selectedText;
         }
     }
 }
