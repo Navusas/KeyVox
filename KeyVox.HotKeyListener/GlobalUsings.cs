0810c04a095e7449b5d8dc95b04a4758db514986
Navusas <D.Giedraitis7@gmail.com>
2023-10-06 15:27:39 +0100

    Remove some projects

--- KeyVox.HotKeyListener/GlobalUsings.cs
+++
@@ -1,2 +0,0 @@
-global using KeyVox.HotKeyListener;
-global using KeyVox.HotKeyListener.HotKeyServices;
--- KeyVox.HotKeyListener/HotKeyServices/IHotKeyService.cs
+++
@@ -1,8 +0,0 @@
-namespace KeyVox.HotKeyListener.HotKeyServices;
-
-public interface IHotKeyService
-{
-    bool TryRegisterHotKey();
-    void UnregisterHotKey();
-    event EventHandler HotKeyPressed;
-}
