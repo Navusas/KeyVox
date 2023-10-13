bf3e16f9e620a75d8402db78924e5a2d528ff664
Navusas <D.Giedraitis7@gmail.com>
2023-10-13 15:59:20 +0100

    fix this error

--- ChatBot/KeyVox.Api/ElectronBootstrap.cs
+++ ChatBot/KeyVox.Api/ElectronBootstrap.cs
@@ -1,11 +1,1 @@
-using ElectronNET.API;
-
-public static class ElectronBoostrap
-{
-    private static async void Bootstrap()
-    {
-        var browserWindow = await Electron.WindowManager.CreateWindowAsync();
-        browserWindow.OnReadyToShow += () => browserWindow.Show();
-        browserWindow.SetTitle("Electron.NET + Svelte");
-    }
-}
+c
