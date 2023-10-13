4743e213e62e24536fdf50bc1d570590a79e6f78
Navusas <D.Giedraitis7@gmail.com>
2023-10-13 15:49:57 +0100

    fix .sln

--- ChatBot/KeyVox.Api/ElectronBootstrap.cs
+++ ChatBot/KeyVox.Api/ElectronBootstrap.cs
@@ -1,10 +1,11 @@
-public static class ElectronBoostrap {
-    public static void Do() {
-        private async void ElectronBootstrap()
+using ElectronNET.API;
+
+public static class ElectronBoostrap
 {
-            var browserWindow = await Electron.WindowManager.CreateWindowAsync();
-            browserWindow.OnReadyToShow += () => browserWindow.Show();
-            browserWindow.SetTitle("Electron.NET + Svelte");
-        }
+    private static async void Bootstrap()
+    {
+        var browserWindow = await Electron.WindowManager.CreateWindowAsync();
+        browserWindow.OnReadyToShow += () => browserWindow.Show();
+        browserWindow.SetTitle("Electron.NET + Svelte");
     }
 }
