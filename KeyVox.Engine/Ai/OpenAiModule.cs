db570e97644cde6cae053450f53e75fb36852c4c
Navusas <D.Giedraitis7@gmail.com>
2023-10-13 15:32:15 +0100

    And create module

---
+++ KeyVox.Engine/Ai/OpenAiModule.cs
@@ -0,0 +1,38 @@
+﻿using KeyVox.Engine.Ai.Providers;
+using KeyVox.Engine.SpeechRecognition;
+using KeyVox.Engine.SpeechRecognition.SpeechToText;
+using KeyVox.Engine.SpeechRecognition.SpeechToText.Providers;
+using KeyVox.Engine.SpeechRecognition.TextToSpeech;
+using KeyVox.Engine.SpeechRecognition.TextToSpeech.Providers;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI;
+using OpenAI.Managers;
+
+namespace KeyVox.Engine.Ai;
+
+public static class OpenAiModule
+{
+    /// <summary>
+    /// Injects <seealso cref="ISpeechToTextClient"/> and <seealso cref="ITextToSpeechClient"/> alongside their compositor
+    /// <seealso cref="SpeechRecognitionClient"/>
+    /// </summary>
+    /// <param name="services"></param>
+    /// <exception cref="ArgumentException"></exception>
+    static void AddSpeechRecognition(this IServiceCollection services)
+    {
+
+        var apiKey = Environment.GetEnvironmentVariable("KEYVOX_AZ_AI_API_KEY") ?? 
+                        throw new ArgumentException("Azure Open AI API Key not found");
+        
+        var openAiService = new OpenAIService(new OpenAiOptions()
+        {
+            ApiKey = apiKey,
+            ApiVersion = "2023-08-01-preview",
+            DeploymentId = "gpt4-32",
+            ResourceName = "redgate-ai",
+            ProviderType = ProviderType.Azure
+        });
+        
+        services.AddSingleton<IOpenAiClient>(_ => new AzureOpenAiClient(openAiService));
+    }
+}
