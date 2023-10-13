6157def7d1f7b41d1352a313803bf2dbee6cbc35
Navusas <D.Giedraitis7@gmail.com>
2023-10-13 16:07:48 +0100

    Recover a file

--- ChatBot/KeyVox.Api/ElectronBootstrap.cs
+++ ChatBot/KeyVox.Api/ElectronBootstrap.cs
@@ -1,1 +1,11 @@
-c
+using ElectronNET.API;
+
+public static class ElectronBoostrap
+{
+    private static async void Bootstrap()
+    {
+        var browserWindow = await Electron.WindowManager.CreateWindowAsync();
+        browserWindow.OnReadyToShow += () => browserWindow.Show();
+        browserWindow.SetTitle("Electron.NET + Svelte");
+    }
+}
